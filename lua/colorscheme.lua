505d7de59d94e610e03d43183e21e88e25f7f764
zhangyongjiang <zhangyongjiang@agoldcomm.com>
2026-04-29 16:35:45 +0800

    update nvim colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,13 +1,17 @@
 -- only store the color scheme configuration.
 return {
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("tokyonight").setup({
-		  style = "moon",
+		require("catppuccin").setup({
+		  flavour = "latte", -- latte, frappe, macchiato, mocha
+		  background = { -- :h background
+		    light = "latte",
+		    dark = "mocha",
+		  },
 		})
 		-- load the colorscheme here
-		vim.cmd.colorscheme("tokyonight")
+		vim.cmd.colorscheme("catppuccin-nvim")
 	end,
 }
